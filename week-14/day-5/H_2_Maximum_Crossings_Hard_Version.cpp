3eb3558b2fdff6b6a6ae7f22b730817d0628e3e3
MahfuzAhmed28 <mahfuzyasin28@gmail.com>
2025-02-02 18:30:58 +0600

    Add files via upload
    week-14 day-5 code

---
+++ week-14/day-5/H_2_Maximum_Crossings_Hard_Version.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define ll long long int
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const long long int INF=1e18+5;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> v(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>v[i];
+        }
+        ll ans=0;
+        pbds<int> p;
+        for(int i=n-1;i>=0;i--)
+        {
+            ans+=p.order_of_key(v[i]+1);
+            p.insert(v[i]);
+        }
+        cout<<ans<<'\n';
+    }
+    return 0;
+}
